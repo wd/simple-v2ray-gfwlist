a629696c6091c3c7e9371a55ee5543567ef6b415
wd <wd@wdicc.com>
2019-09-02 14:24:19 +0800

    增加一些测试用例

--- tests/test_svgfw.py
+++ tests/test_svgfw.py
@@ -1,15 +1,51 @@
 import re
 from utils import import_path
+import unittest
+import os
+import configparser
+import json
 
 svgfw = import_path('./src/svgfw')
+GFW = svgfw.GFW
+Dnsmasq = svgfw.Dnsmasq
+Iptable = svgfw.Iptable
 
 
-def test_process_gfw_list():
-    gfw = svgfw.GFW()
-    domain_pattern = r'^([a-z0-9][a-z0-9-]*\.)*[a-z0-9][a-z0-9-]{0,61}\.(xn--[a-z0-9]+|[a-z]{2,})$'
-    with open('tests/gfwlist.txt') as fh:
-        domains = gfw._process_gfw_list(fh.read())
+class GFWTest(unittest.TestCase):
+    def test_process_gfw_list(self):
+        gfw = GFW()
+        domain_pattern = r'^([a-z0-9][a-z0-9-]*\.)*[a-z0-9][a-z0-9-]{0,61}\.(xn--[a-z0-9]+|[a-z]{2,})$'
+        with open('tests/gfwlist.txt') as fh:
+            domains = gfw._process_gfw_list(fh.read())
 
-    for domain in domains:
-        m = re.match(domain_pattern, domain.lower())
-        assert m is not None, "{} is not a valid domain".format(domain)
+        for domain in domains:
+            m = re.match(domain_pattern, domain.lower())
+            assert m is not None, "{} is not a valid domain".format(domain)
+
+    def test_init(self):
+        config_file = os.path.dirname(os.path.abspath(__file__)) + '/config.ini'
+        config = open(config_file).read()
+        gfw = GFW(config)
+
+        self.assertIsInstance(gfw.dnsmasq, Dnsmasq)
+        self.assertIsInstance(gfw.iptable, Iptable)
+        self.assertEqual(gfw.config_err, [])
+
+        conf = configparser.ConfigParser()
+        conf.read_string(config)
+        conf['v2ray']['outbounds'] = json.dumps([])
+
+        class ConfigToString():
+            def __init__(self):
+                self.lines = []
+
+            def write(self, data):
+                self.lines.append(data)
+
+        cts = ConfigToString()
+        conf.write(cts)
+
+        gfw = GFW("".join(cts.lines))
+        self.assertEqual(gfw.config_err,
+                         ['Outbound tag for "netflix" not found',
+                          'Outbound tag for "gfwlist" not found'])
--- tests/test_svgfw_Utils.py
+++ tests/test_svgfw_Utils.py
@@ -1,44 +1,137 @@
 from utils import import_path
 import tempfile
+import os
+import threading
+import socket
+import unittest
+import time
+import logging
 
 svgfw = import_path('./src/svgfw')
+fake_bin_path = os.path.dirname(os.path.abspath(__file__)) + '/bin'
+Utils = svgfw.Utils
+Utils.logger.setLevel(logging.DEBUG)
 
 
-def test_get_command_version():
-    args = ['v2ray', '--version']
-    regex = r'V2Ray (\d)\.(\d+)\.(\d+)'
-    version = svgfw.Utils.get_command_version(args, regex)
+class UtilsTest(unittest.TestCase):
+    def test_get_command_version(self):
+        cmd = svgfw.Utils.which('v2ray', path_list=[fake_bin_path])
+        args = [cmd, '--version']
+        regex = r'V2Ray (\d)\.(\d+)\.(\d+)'
+        version = svgfw.Utils.get_command_version(args, regex)
 
-    assert version == [4, 20, 0]
+        self.assertEqual(version, [4, 20, 0])
 
+    def test_str_to_list(self):
+        t = {
+            '1.1.1.1': ['1.1.1.1'],
+            '1.1.1.1, 2.3.2.1': ['1.1.1.1', '2.3.2.1'],
+            '1.1.1.1; 2.3.2.1': ['1.1.1.1', '2.3.2.1'],
+            '1.1.1.1;2.3.2.1': ['1.1.1.1', '2.3.2.1']
+        }
 
-def test_str_to_list():
-    t = {
-        '1.1.1.1': ['1.1.1.1'],
-        '1.1.1.1, 2.3.2.1': ['1.1.1.1', '2.3.2.1'],
-        '1.1.1.1; 2.3.2.1': ['1.1.1.1', '2.3.2.1'],
-        '1.1.1.1;2.3.2.1': ['1.1.1.1', '2.3.2.1']
-    }
+        for ips, expect in t.items():
+            res = Utils.str_to_list(ips)
+            self.assertEqual(res, expect)
 
-    for ips, expect in t.items():
-        res = svgfw.Utils.str_to_list(ips)
-        assert res == expect
+    def test_split_ip_port(self):
+        default_port = 53
+        t = {
+            '1.1.1.1': ('1.1.1.1', default_port),
+            '1.1.1.1:34': ('1.1.1.1', 34)
+        }
+        for ip, expect in t.items():
+            res = Utils.split_ip_port(ip, default_port)
+            self.assertEqual(res, expect)
 
+    def test_read_file_to_list(self):
+        tmpfile = tempfile.NamedTemporaryFile(delete=True)
+        lines = ["xxxx", "ffffffffffffff"]
+        tmpfile.write("\n".join(lines).encode('utf-8'))
+        tmpfile.flush()
+        self.assertEqual(lines, Utils.read_file_to_list(tmpfile.name))
 
-def test_split_ip_port():
-    default_port = 53
-    t = {
-        '1.1.1.1': ('1.1.1.1', default_port),
-        '1.1.1.1:34': ('1.1.1.1', 34)
-    }
-    for ip, expect in t.items():
-        res = svgfw.Utils.split_ip_port(ip, default_port)
-        assert res == expect
+    def test_which(self):
+        cmd = Utils.which('iptables', path_list=[fake_bin_path])
+        self.assertEqual(cmd, fake_bin_path + '/iptables')
 
+    def test_run_command(self):
+        cmd = Utils.which('test_command', path_list=[fake_bin_path])
+        tmpfile = tempfile.NamedTemporaryFile(delete=True)
+        test_str = 'test'
+        code = Utils.run_command(cmd, [cmd, 'output', test_str], stdout=tmpfile)
+        self.assertEqual(code, 0, 'success exit code')
+        tmpfile.seek(0)
+        self.assertEqual(test_str, tmpfile.read().decode('utf8').rstrip(), 'output to a file')
 
-def test_read_file_to_list():
-    tmpfile = tempfile.NamedTemporaryFile(delete=True)
-    lines = ["xxxx", "ffffffffffffff"]
-    tmpfile.write("\n".join(lines).encode('utf-8'))
-    tmpfile.flush()
-    assert lines == svgfw.Utils.read_file_to_list(tmpfile.name)
+        code = Utils.run_command(cmd, [cmd, 'error_quit'])
+        self.assertGreater(code, 0, 'faild exit code')
+
+        def check_func(p):
+            return p.pid
+
+        pid = Utils.run_command(cmd, [cmd], background=True, check_func=check_func)
+        self.assertGreater(pid, 0, 'background run, and check function')
+
+    def test_is_port_open(self):
+        port = 23456
+        self.assertFalse(Utils.is_port_open(port))
+
+        def open_port(port):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                try:
+                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    sock.bind(('127.0.0.1', port))
+                    sock.listen(1)
+                    sock.accept()
+                except Exception:
+                    print('bind faild')
+
+        t = threading.Thread(target=open_port, args=[port])
+        t.start()
+
+        self.assertTrue(Utils.is_port_open(port))
+        t.join()
+
+    def test_get_pid(self):
+        with tempfile.NamedTemporaryFile(delete=True) as fh:
+            pid = 1923
+            fh.write(str(pid).encode('utf8'))
+            fh.seek(0)
+            self.assertEqual(Utils.get_pid(fh.name), pid)
+
+    def test_check_log_output_linecount(self):
+        _, temp_file = tempfile.mkstemp()
+        fh = open(temp_file, 'w+')
+
+        def write_log():
+            time.sleep(2)
+            fh.write('11111')
+            fh.seek(0)
+
+        t = threading.Thread(target=write_log)
+        t.start()
+        fh.seek(0)
+
+        self.assertTrue(Utils.check_log_output(fh.name, linecount=1))
+        t.join()
+        fh.close()
+        os.unlink(temp_file)
+
+    def test_check_log_output_regex(self):
+        _, temp_file = tempfile.mkstemp()
+        fh = open(temp_file, 'w+')
+
+        def write_log():
+            time.sleep(2)
+            fh.write('11111')
+            fh.seek(0)
+
+        t = threading.Thread(target=write_log)
+        t.start()
+        fh.seek(0)
+
+        self.assertTrue(Utils.check_log_output(fh.name, regex=r'11111'))
+        t.join()
+        fh.close()
+        os.unlink(temp_file)
